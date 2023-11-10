0d7ad2e9b56d595286f1bfbc43b1b36df6c54c76
Michał Pawlak <michal.pawlak001@gmail.com>
2023-11-10 14:33:21 +0100

    create playRound and game functions

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ function getComputerChoice(){
             return "Scissors";
     }
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toUpperCase()===computerSelection.toUpperCase()){
+        return `It's a ${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase()} to ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1).toLowerCase()} draw`;
+    }else if(((playerSelection.toUpperCase()==="ROCK")&&(computerSelection.toUpperCase()==="SCISSORS"))||((playerSelection.toUpperCase()==="SCISSORS")&&(computerSelection.toUpperCase()==="PAPER"))||((playerSelection.toUpperCase()==="PAPER")&&(computerSelection.toUpperCase()==="ROCK"))){
+        return `${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase()} beats ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1).toLowerCase()}, you win!`;
+    }else if(((playerSelection.toUpperCase()==="ROCK")&&(computerSelection.toUpperCase()==="PAPER"))||((playerSelection.toUpperCase()==="SCISSORS")&&(computerSelection.toUpperCase()==="ROCK"))||((playerSelection.toUpperCase()==="PAPER")&&(computerSelection.toUpperCase()==="SCISSORS"))){
+        return `${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1).toLowerCase()} beats ${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase()}, you lose!`;
+    }else{
+        return "Invalid input!";
+    }
+}
+
+function game(){
+    let compChoice = getComputerChoice();
+    let playerChoice = prompt("Rock, Paper or Scissors?");
+    console.log(playRound(playerChoice, compChoice));
+}
